1d5316a541260b5c17965c6a1bfb863746cfd432
botagoz <botagoz.aimkulova@gmail.com>
2020-12-11 20:12:00 +0600

    #1 Добавлены второй и третий блок страницы и медиазапросы

--- js/script.js
+++ js/script.js
@@ -4,7 +4,35 @@ $(() => {
   });
   $(document).ready(function () {
     $(".slider").slick({
-      
     });
+  });
+  $('.content_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
